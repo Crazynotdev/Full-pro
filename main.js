9024c1b9de0a96a7ee92919045b7cf933c755fb6
DavidTechInc <techigwe@gmail.com>
2025-06-19 16:50:34 +0100

    Delete main.js

--- main.js
+++
@@ -1,5 +0,0 @@
-document.getElementById('create').onclick = () => {
-  const u = document.getElementById('username').value.trim();
-  if (!u) return alert('Entre un pseudo');
-  location.href = `user.html?user=${encodeURIComponent(u)}`;
-};
